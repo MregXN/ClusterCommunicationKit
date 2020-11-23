1cd239942ddabbb0837e64a7e9bd364b4b0d9e95
mregxn <mregxn@gmail.com>
2020-11-23 05:10:54 -0800

    accomplish UI of client

--- Makefile
+++ Makefile
@@ -1,25 +1,28 @@
 MUDUO_DIRECTORY ?= $(PWD)/muduo
 MUDUO_INCLUDE = $(MUDUO_DIRECTORY)/include
 MUDUO_LIBRARY = $(MUDUO_DIRECTORY)/lib
+MUDUO_CODEC_PATH =  $(MUDUO_DIRECTORY)/codec
+MUDUO_PUBSUB_PATH =  $(MUDUO_DIRECTORY)/pubsub
+
 
 CXXFLAGS = -g -O0 -Wall -Wextra -Werror \
 	   -Wconversion -Wno-unused-parameter \
 	   -Wold-style-cast -Woverloaded-virtual \
 	   -Wpointer-arith -Wshadow -Wwrite-strings \
 	   -march=native -rdynamic \
-	   -I$(MUDUO_INCLUDE)
+	   -I$(MUDUO_INCLUDE) -I$(MUDUO_CODEC_PATH) -I$(MUDUO_PUBSUB_PATH)
 
-LDFLAGS = -L$(MUDUO_LIBRARY) -lmuduo_net -lmuduo_base -lpthread -lrt
+LDFLAGS = -L$(MUDUO_LIBRARY)  -lmuduo_net -lmuduo_base -lpthread -lrt 
 
 all: client server
 
 clean:
 	rm -f client server
 
-client: src/client/client.cc
+client: src/client/client.cc muduo/codec/codec.cc muduo/pubsub/pubsub.cc
 	g++ $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
 
-server: src/server/server.cc
+server: src/server/server.cc muduo/codec/codec.cc muduo/pubsub/pubsub.cc
 	g++ $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
 
 .PHONY: all clean
--- src/client/client.cc
+++ src/client/client.cc
@@ -1,4 +1,5 @@
-#include "muduo/base/codec.h"
+#include "pubsub.h"
+#include "codec.h"
 
 #include "muduo/base/Logging.h"
 #include "muduo/base/Mutex.h"
@@ -8,96 +9,93 @@
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 using namespace muduo;
 using namespace muduo::net;
+using namespace pubsub;
 
-class ChatClient : noncopyable
-{
- public:
-  ChatClient(EventLoop* loop, const InetAddress& serverAddr)
-    : client_(loop, serverAddr, "ChatClient"),
-      codec_(std::bind(&ChatClient::onStringMessage, this, _1, _2, _3))
-  {
-    client_.setConnectionCallback(
-        std::bind(&ChatClient::onConnection, this, _1));
-    client_.setMessageCallback(
-        std::bind(&LengthHeaderCodec::onMessage, &codec_, _1, _2, _3));
-    client_.enableRetry();
-  }
+EventLoop* g_loop = NULL;
+std::vector<string> g_topics;
+string g_content;
 
-  void connect()
-  {
-    client_.connect();
-  }
+// void subscription(const string& topic, const string& content, Timestamp)
+// {
+//   printf("%s: %s\n", topic.c_str(), content.c_str());
+// }
 
-  void disconnect()
-  {
-    client_.disconnect();
-  }
+// void connection(PubSubClient* client)
+// {
+//   if (client->connected())
+//   {
+//     for (std::vector<string>::iterator it = g_topics.begin();
+//         it != g_topics.end(); ++it)
+//     {
+//       client->subscribe(*it, subscription);
+//     }
+//   }
+//   else
+//   {
+//     g_loop->quit();
+//   }
+// }
 
-  void write(const StringPiece& message)
-  {
-    MutexLockGuard lock(mutex_);
-    if (connection_)
-    {
-      codec_.send(get_pointer(connection_), message);
-    }
-  }
+void message()
+{}
 
- private:
-  void onConnection(const TcpConnectionPtr& conn)
-  {
-    LOG_INFO << conn->localAddress().toIpPort() << " -> "
-             << conn->peerAddress().toIpPort() << " is "
-             << (conn->connected() ? "UP" : "DOWN");
+void fileTransfer()
+{}
 
-    MutexLockGuard lock(mutex_);
-    if (conn->connected())
-    {
-      connection_ = conn;
-    }
-    else
-    {
-      connection_.reset();
-    }
-  }
-
-  void onStringMessage(const TcpConnectionPtr&,
-                       const string& message,
-                       Timestamp)
-  {
-    printf("<<< %s\n", message.c_str());
-  }
-
-  TcpClient client_;
-  LengthHeaderCodec codec_;
-  MutexLock mutex_;
-  TcpConnectionPtr connection_ GUARDED_BY(mutex_);
-};
 
 int main(int argc, char* argv[])
 {
-  LOG_INFO << "pid = " << getpid();
-  if (argc > 2)
+  if (argc != 2)
   {
-    EventLoopThread loopThread;
-    uint16_t port = static_cast<uint16_t>(atoi(argv[2]));
-    InetAddress serverAddr(argv[1], port);
+    printf("Usage: ./client [server_ip]:[port] \n");
+    return 0;
+  }
 
-    ChatClient client(loopThread.startLoop(), serverAddr);
-    client.connect();
-    std::string line;
-    while (std::getline(std::cin, line))
-    {
-      client.write(line);
-    }
-    client.disconnect();
-    CurrentThread::sleepUsec(1000*1000);  // wait for disconnect, see ace/logging/client.cc
-  }
-  else
+  printf("please input your hostname:  \n");
+  string name;
+  getline(std::cin,name);
+
+  printf("\033[2J");
+  printf("welcome %s \n",name.c_str());
+  printf("\n");
+  printf("please input number to choose funtions: \n");
+  printf("-------------------------------------- \n");
+  printf("1. message  \n");
+  printf("2. file transfer \n");
+  printf("0. quit \n");
+  printf("-------------------------------------- \n");
+
+  string cmd;
+  getline(std::cin,cmd);
+
+  switch(cmd[0])
   {
-    printf("Usage: %s host_ip port\n", argv[0]);
+    case '1': 
+      message();
+    break;
+
+    case '2': 
+      fileTransfer();
+    break;
+
+    case '0': 
+      return 0;
+    break;
+
   }
+
+
+  // string hostport = argv[1];
+  // size_t colon = hostport.find(':');
+  // if (colon != string::npos)
+  // {
+  //   string hostip = hostport.substr(0, colon);
+  //   uint16_t port = static_cast<uint16_t>(atoi(hostport.c_str()+colon+1));
+  // }
+
+
 }
-
